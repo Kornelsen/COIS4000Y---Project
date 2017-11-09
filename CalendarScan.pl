53bfda10a6d345e68159293889c3b97b1be04d58
Matt Kornelsen <kornelsen@Matts-MacBook-Pro.local>
2017-11-09 16:30:05 -0500

    Perl Regex Working

--- CalendarScan.pl
+++ CalendarScan.pl
@@ -1,6 +1,34 @@
 #!usr/bin/perl
 
 #Capture course text [»]\s(.*)[»]
-#Capture Prequisites: \bPrerequisite\b(.*)[.]
+#Capture Prequisites: \bPrerequisite\b(.*)[./s]
 #Capture Course code: [A-Z]{4}\s[0-9]{4}
+
+my $input_file = "Calendar.txt";
+
+open my $fh, '<', $input_file or die "Could not open file";
+
+my $file_content = do { local $/; <$fh> };
+
+my @courses = ($file_content =~ /COURSE>\s+(.+?)</sg);
+
+my $regex = qr/[A-Z]{4}\s[0-9]{4}(H|Y){1}/p;
+my $regex2 = qr/\bPrerequisite\b(.*)[.]/sp;
+
+
+for (my $i = 0; $i < @courses; $i++) {
+
+    if ( $courses[$i] =~ /$regex/ ) {
+  		print "<COURSE> ${^MATCH}\n";
+  	}
+
+  	if ( $courses[$i] =~ /$regex2/ ) {
+  		print "\t<PREREQ> ${^MATCH} </PREREQ>\n\n";
+  	}
+
+  	print "</COURSE>\n\n"
+}
+
+
+
 
